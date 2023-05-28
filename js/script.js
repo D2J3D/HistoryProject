2da5759aa8e1c3233b078684b89f0540253865fe
D2J3D <120342275+D2J3D@users.noreply.github.com>
2023-05-28 14:48:28 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+let i = 0;
+document.getElementById('working_mode').addEventListener('click', function (e){
+    if(i % 2 === 0){
+        document.getElementById('main_hints').style.display = 'block';
+        document.getElementById('new_hints').style.display = 'block';
+        document.getElementById('new_hints2').style.display = 'block';
+        document.getElementById('working_mode').innerHTML = "Скрыть подробности";
+        i++;
+    } else{
+        document.getElementById('main_hints').style.display = 'none';
+        document.getElementById('new_hints').style.display = 'none';
+        document.getElementById('new_hints2').style.display = 'none';
+        document.getElementById('working_mode').innerHTML = "Сделать подробнее";
+        i++;
+    }
+});
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
